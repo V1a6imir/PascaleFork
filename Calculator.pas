c9b6cbc27b1c08e68e5efd63da1c8a037ab6db42
Vladimir Monastirsky <monstrmonstr2003@gmail.com>
2024-07-28 16:15:28 +0300

    Fix error

--- Calculator.pas
+++ Calculator.pas
@@ -1,4 +1,4 @@
-﻿program gg;
+program gg;
 
 var
   x1, x2, o: real;
@@ -19,5 +19,5 @@ begin
     o := x1 * x2;
   if (ch = '/') and (x2 <> 0) then 
     o := x1 / x2;
-  write(' Ответ: ', o:2:1);
+  write(' Ответ: ');
 end.
